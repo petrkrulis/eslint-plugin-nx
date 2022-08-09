8742d87f608af00eb134fcb7cdf9b907036b578a
Petr Kruliš <hello@petrkrulis.cz>
2022-08-09 18:43:40 +0200

    constants - need to run

--- constants.js
+++ constants.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WORKSPACE_RULE_NAMESPACE = exports.WORKSPACE_PLUGIN_DIR = void 0;
-const devkit_1 = require("@nrwl/devkit");
+const app_root_1 = require("./app-root");
 const path_1 = require("path");
-exports.WORKSPACE_PLUGIN_DIR = (0, path_1.join)(devkit_1.workspaceRoot, 'tools/eslint-rules');
+exports.WORKSPACE_PLUGIN_DIR = (0, path_1.join)(app_root_1.appRootPath, 'tools/eslint-rules');
 /**
  * We add a namespace so that we mitigate the risk of rule name collisions as much as
  * possible between what users might create in their workspaces and what we might want
